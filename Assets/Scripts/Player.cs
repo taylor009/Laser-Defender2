fa792f0441dc5b5a7eb5fc6fbf316d814968c05a
Taylor Gagne <taylor@baremetalcloud.com>
2021-06-12 04:18:29 -0400

    Added view port to world point.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // config params
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float padding = 1f;
+
+    private float xMin;
+    private float xMax;
+
+    private float yMin;
+    private float yMax;
     
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        var gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
         
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -23,8 +41,9 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
